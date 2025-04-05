fcf9d68c93c1224ed7901402ded3e06c9a71499c
kkachi9 <127672999+kkachi9@users.noreply.github.com>
2025-04-05 22:40:04 +0900

    1.8

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/mobile-wedding-invitation/', // 예시: 저장소 이름이 my-react-app 이면 /my-react-app/
   plugins: [react(), tsconfigPaths(), svgr()],
 });
