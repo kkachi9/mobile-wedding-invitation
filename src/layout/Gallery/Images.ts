69bc82570bb5709875b266a9af8b7ac74211efb6
kkachi9 <127672999+kkachi9@users.noreply.github.com>
2025-04-05 21:57:24 +0900

    1.1

--- src/layout/Gallery/Images.ts
+++ src/layout/Gallery/Images.ts
@@ -1,15 +1,15 @@
-import image01 from '@/assets/images/01.jpg'
-import image02 from '@/assets/images/02.jpg'
-import image03 from '@/assets/images/03.jpg'
-import image04 from '@/assets/images/04.jpg'
-import image05 from '@/assets/images/05.jpg'
-import image06 from '@/assets/images/06.jpg'
-import image07 from '@/assets/images/07.jpg'
-import image08 from '@/assets/images/08.jpg'
-import image09 from '@/assets/images/09.jpg'
-import image10 from '@/assets/images/10.jpg'
-import image11 from '@/assets/images/11.jpg'
-import image12 from '@/assets/images/12.jpg'
+import image01 from '@/assets/images/1.jpg'
+import image02 from '@/assets/images/2.jpg'
+import image03 from '@/assets/images/3.jpg'
+import image04 from '@/assets/images/4.jpg'
+import image05 from '@/assets/images/5.jpg'
+import image06 from '@/assets/images/6.jpg'
+import image07 from '@/assets/images/7.jpg'
+// import image08 from '@/assets/images/08.jpg'
+// import image09 from '@/assets/images/09.jpg'
+// import image10 from '@/assets/images/10.jpg'
+// import image11 from '@/assets/images/11.jpg'
+//import image12 from '@/assets/images/12.jpg'
 
 const images = [   /* 이미지 경로를 리스트로 저장 */
   {
@@ -47,31 +47,6 @@ const images = [   /* 이미지 경로를 리스트로 저장 */
   },{
     alt: 'image07',
     source: image07,
-    width: 640,
-    height: 960
-  },{
-    alt: 'image08',
-    source: image08,
-    width: 640,
-    height: 960
-  },{
-    alt: 'image09',
-    source: image09,
-    width: 640,
-    height: 960
-  },{
-    alt: 'image10',
-    source: image10,
-    width: 640,
-    height: 960
-  },{
-    alt: 'image11',
-    source: image11,
-    width: 640,
-    height: 960
-  },{
-    alt: 'image12',
-    source: image12,
     width: 640,
     height: 960
   }
